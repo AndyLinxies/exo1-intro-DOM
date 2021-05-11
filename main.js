15cc18695cbaa9f4b4819af9a62a1704ec2c7854
AndyLinxies <linxies@gmail.com>
2021-05-11 10:29:03 +0200

    intro exo1 DOM

---
+++ main.js
@@ -0,0 +1,20 @@
+//Ex1 Recupère tous les h1
+let tousH1 = document.getElementsByTagName('h1');
+console.log(tousH1);
+
+//Ex2
+
+let premH3 = document.getElementsByTagName('h3')[0];
+let deuxH3 = document.getElementsByTagName('h3')[1];
+let troisH3 = document.getElementsByTagName('h3')[2];
+console.log(premH3,deuxH3,troisH3);
+
+//Ex3
+let tousP = document.getElementsByTagName('p');
+let par2= tousP[1];
+console.log(par2);
+
+//Ex4
+let tousLi = document.getElementsByTagName('li');
+let li3 = tousLi[2];
+console.log(li3);
